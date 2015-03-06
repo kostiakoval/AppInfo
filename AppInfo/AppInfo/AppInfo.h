249ac8ed1c8fb4ca241ae9a61d9b5fd720652471
Konstantin Koval <konstantin@rocketfarm.no>
2015-03-06 22:38:48 +0100

    add AppInfo framework

---
+++ AppInfo/AppInfo/AppInfo.h
@@ -0,0 +1,19 @@
+//
+//  AppInfo.h
+//  AppInfo
+//
+//  Created by Konstantin Koval on 06/03/15.
+//  Copyright (c) 2015 Kostiantyn Koval. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AppInfo.
+FOUNDATION_EXPORT double AppInfoVersionNumber;
+
+//! Project version string for AppInfo.
+FOUNDATION_EXPORT const unsigned char AppInfoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppInfo/PublicHeader.h>
+
+
