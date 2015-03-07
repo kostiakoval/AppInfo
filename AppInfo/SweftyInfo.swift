619aca0a210e1f42dca90e43f6449a0508f793d0
Konstantin Koval <konstantin@rocketfarm.no>
2015-03-07 18:50:28 +0100

    make stuff public

--- AppInfo/SweftyInfo.swift
+++ AppInfo/SweftyInfo.swift
@@ -30,87 +30,89 @@ private func array<T>(object: AnyObject?) -> Array<T>? {
 }
 
 public struct AppInfo {
-  static let bundleInfo = NSBundle.mainBundle().infoDictionary as! Dictionary<String, AnyObject>
+  private static let bundleInfo = NSBundle.mainBundle().infoDictionary as! Dictionary<String, AnyObject>
 
   static var CFBundleIdentifier: String? {
     return bundleInfo["CFBundleIdentifier"] >> string
   }
 
   public static var DTPlatformName: String? {
-   return bundleInfo["DTPlatformName"] >> string
+    return bundleInfo["DTPlatformName"] >> string
   }
 
-  static var UIMainStoryboardFile: String? {
+  public static var UIMainStoryboardFile: String? {
     return bundleInfo["UIMainStoryboardFile"] >> string
   }
-  static var CFBundleVersion: Int? {
+  public static var CFBundleVersion: Int? {
     return bundleInfo["CFBundleVersion"] >> int
   }
 
-  static var CFBundleSignature: String? {
+  public static var CFBundleSignature: String? {
     return bundleInfo["CFBundleSignature"] >> string
   }
 
-//  static var CFBundleIdentifier: String? {
-//    return bundleInfo["CFBundleIdentifier"] >> string
-//  }
-
-  static var CFBundleExecutable: String? {
+  public static var CFBundleExecutable: String? {
     return bundleInfo["CFBundleExecutable"] >> string
   }
-  static var LSRequiresIPhoneOS: Int? {
+
+  public static var LSRequiresIPhoneOS: Int? {
     return bundleInfo["LSRequiresIPhoneOS"] >> int
   }
 
-  static var CFBundleName: String? {
+  public static var CFBundleName: String? {
     return bundleInfo["CFBundleName"] >> string
   }
 
-  static var UILaunchStoryboardName: String? {
+  public static var UILaunchStoryboardName: String? {
     return bundleInfo["UILaunchStoryboardName"] >> string
   }
 
-  static var CFBundleSupportedPlatforms: Array<String>? {
+  public static var CFBundleSupportedPlatforms: Array<String>? {
     return bundleInfo["CFBundleSupportedPlatforms"] >> array
   }
-  static var CFBundlePackageType: String? {
+
+  public static var CFBundlePackageType: String? {
     return bundleInfo["CFBundlePackageType"] >> string
   }
-  static var CFBundleNumericVersion: Int? {
+
+  public static var CFBundleNumericVersion: Int? {
     return bundleInfo["CFBundleNumericVersion"] >> int
   }
 
-  static var CFBundleInfoDictionaryVersion: String? {
+  public static var CFBundleInfoDictionaryVersion: String? {
     return bundleInfo["CFBundleInfoDictionaryVersion"] >> string
   }
 
-  static var UIRequiredDeviceCapabilities: Array<String>? {
+  public static var UIRequiredDeviceCapabilities: Array<String>? {
     return bundleInfo["UIRequiredDeviceCapabilities"] >> array
   }
 
-  static var UISupportedInterfaceOrientations: Array<String>? {
-  return bundleInfo["UISupportedInterfaceOrientations"] >> array
+  public static var UISupportedInterfaceOrientations: Array<String>? {
+    return bundleInfo["UISupportedInterfaceOrientations"] >> array
   }
 
-  static var CFBundleInfoPlistURL: String? {
-  return bundleInfo["CFBundleInfoPlistURL"] >> string
+  public static var CFBundleInfoPlistURL: String? {
+    return bundleInfo["CFBundleInfoPlistURL"] >> string
   }
 
-  static var CFBundleDevelopmentRegion: String? {
+  public static var CFBundleDevelopmentRegion: String? {
     return bundleInfo["CFBundleDevelopmentRegion"] >> string
   }
-  static var DTSDKName: String? {
-  return bundleInfo["DTSDKName"] >> string
+
+  public static var DTSDKName: String? {
+    return bundleInfo["DTSDKName"] >> string
   }
-  static var UIDeviceFamily: Array<Int>? {
-  return bundleInfo["UIDeviceFamily"] >> array
+
+  public static var UIDeviceFamily: Array<Int>? {
+    return bundleInfo["UIDeviceFamily"] >> array
   }
-  static var CFBundleShortVersionString: String? {
+
+  public static var CFBundleShortVersionString: String? {
     return bundleInfo["CFBundleShortVersionString"] >> string
   }
 
   /*static var <#name#>: <#Type#> {
-    return bundleInfo[<#name#>] >>
+  return bundleInfo[<#name#>] >>
   } */
 
   public static func info() -> Dictionary<NSObject, AnyObject>? {
